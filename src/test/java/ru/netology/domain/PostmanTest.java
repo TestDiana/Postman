6fea2a3a089dac244cba9fa9e6f3b72cdc6233cc
Diana Vafina <Dianochka.25@mail.ru>
2021-05-26 21:22:16 +0300

    fixed test

--- src/test/java/ru/netology/domain/PostmanTest.java
+++ src/test/java/ru/netology/domain/PostmanTest.java
@@ -14,7 +14,7 @@ public class PostmanTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("bye")
+                .body("привет")
                 .when()
                 .post("/post")
                 .then()
