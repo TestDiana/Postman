b227a9b333ad933ea8b802bedbd45effba320e34
Diana Vafina <Dianochka.25@mail.ru>
2021-05-26 21:19:21 +0300

    failed test

--- src/test/java/ru/netology/domain/PostmanTest.java
+++ src/test/java/ru/netology/domain/PostmanTest.java
@@ -14,10 +14,10 @@ public class PostmanTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("привет")
-                .when()//
+                .body("bye")
+                .when()
                 .post("/post")
-                .then()// 
+                .then()
                 .statusCode(200)
                 .body("data", Matchers.equalTo("привет"))
         ;
