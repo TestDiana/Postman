f036f206762652c2136847f945f079bb224fd6cb
Diana Vafina <Dianochka.25@mail.ru>
2021-05-26 21:08:25 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanTest {
+
+
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")
+                .when()//
+                .post("/post")
+                .then()// 
+                .statusCode(200)
+                .body("data", Matchers.equalTo("привет"))
+        ;
+    }
+}
